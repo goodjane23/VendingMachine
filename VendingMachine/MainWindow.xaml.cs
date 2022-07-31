b37cb19143eeb6d7025dc02fb089e43d86fc6be3
Ekaterina Razimovich <apochemubinet@gmail.com>
2022-07-31 19:31:01 +0300

    add image

--- VendingMachine/MainWindow.xaml.cs
+++ VendingMachine/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VendingMachine.ViewModels;
 
 namespace VendingMachine
 {
@@ -23,6 +24,9 @@ namespace VendingMachine
         public MainWindow()
         {
             InitializeComponent();
+            MachineViewModel locator = new MachineViewModel();
+            DataContext = locator;
+
         }
     }
 }
