7a4ab7fa1118774b59401d76f44ffd567dfcddf1
Nikita Khodyakov <fenyatop@gmail.com>
2022-09-25 20:50:56 +0300

    Delete file

--- VendingMachine/ViewModels/MachineViewModel.cs
+++ VendingMachine/ViewModels/MachineViewModel.cs
@@ -6,48 +6,46 @@ namespace VendingMachine.ViewModels;
 
 public class MachineViewModel : ICommand
 {
-    public class MachineViewModel :  ICommand
+    public string Screen { get; private set; }
+
+    private ICommand _numberClick;
+    private ICommand _okClick;
+    private ICommand _cancelClick;
+    public ICommand NumberClick
     {
-        public string Screen { get; private set; }
+        get { return _numberClick ?? (_numberClick = new RelayCommand<string>(ToScreen)); }
+    }
 
-        private ICommand _numberClick;
-        private ICommand _okClick;
-        private ICommand _cancelClick;
-        public ICommand NumberClick
-        {
-            get { return _numberClick ?? (_numberClick = new RelayCommand<string>(ToScreen)); }
-        }
+    public ICommand OkClick
+    {
+        get { return _okClick ?? (_okClick = new RelayCommand(PressedOkClick)); }
+    }
 
-        public ICommand OkClick
-        {
-            get { return _okClick ?? (_okClick = new RelayCommand(PressedOkClick)); } 
-        }
+    public ICommand CancelClick
+    {
+        get { return _cancelClick ?? (_cancelClick = new RelayCommand(PressedCancelClick)); }
+    }
 
-        public ICommand CancelClick 
-        { 
-            get { return _cancelClick??(_cancelClick = new RelayCommand(PressedCancelClick)); } 
-        }
+    public MachineViewModel()
+    {
 
-        public MachineViewModel()
-        {
-           
-        }
-        
-        private void ToScreen(string num)
-        {
-            Screen += num;
-            if (Screen.Length > 2)
-                return;
-            
-        }
-        private void PressedOkClick()
-        {
-            Screen = "";
-        }
-        private void PressedCancelClick()
-        {
-            Screen = "__";
-        }
+    }
+
+    private void ToScreen(string num)
+    {
+        Screen += num;
+        if (Screen.Length > 2)
+            return;
+
+    }
+    private void PressedOkClick()
+    {
+        Screen = "";
+    }
+    private void PressedCancelClick()
+    {
+        Screen = "__";
+    }
 
 
 
