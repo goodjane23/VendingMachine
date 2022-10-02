1e2d068d93bfb33bde6112e0d34f99bb4f619d34
Nikita Khodyakov <fenyatop@gmail.com>
2022-10-02 23:34:08 +0300

    Setup DI container

--- VendingMachine/App.xaml.cs
+++ VendingMachine/App.xaml.cs
@@ -1,7 +1,32 @@
 ﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using VendingMachine.ViewModels;
+using VendingMachine.Views.Windows;
 
 namespace VendingMachine;
 
 public partial class App : Application
 {
+    private readonly ServiceProvider serviceProvider;
+    
+    public App()
+    {
+        var services = new ServiceCollection();
+        
+        // Windows
+        services.AddSingleton<MainWindow>();
+        
+        //ViewModels
+        services.AddSingleton<MainWindowViewModel>();
+
+        serviceProvider = services.BuildServiceProvider();
+    }
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+
+        var window = serviceProvider.GetRequiredService<MainWindow>();
+        window.Show();
+    }
 }
