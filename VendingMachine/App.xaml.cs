8d397e578ca1bd63a8ada8ad5d3819e817f4de29
goodjane23 <apochemubinet@gmail.com>
2022-10-11 22:07:24 +0300

    add Host, register in DI, create ctor with parametrs

--- VendingMachine/App.xaml.cs
+++ VendingMachine/App.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VendingMachine.Data;
 using VendingMachine.ViewModels;
 using VendingMachine.Views.Windows;
 
@@ -7,26 +10,36 @@ namespace VendingMachine;
 
 public partial class App : Application
 {
-    private readonly ServiceProvider serviceProvider;
+  
+    private readonly IHost appHost;
     
     public App()
     {
         var services = new ServiceCollection();
-        
-        // Windows
-        services.AddSingleton<MainWindow>();
-        
-        //ViewModels
-        services.AddSingleton<MainWindowViewModel>();
 
-        serviceProvider = services.BuildServiceProvider();
+        appHost = Host.CreateDefaultBuilder()
+            .ConfigureServices(services =>
+            {
+                // Windows
+                services.AddSingleton<MainWindow>();
+
+                //ViewModels
+                services.AddSingleton<MainWindowViewModel>();
+                services.AddDbContextFactory<VendingDbContext>(options =>
+                {
+                    options.UseSqlite("Data Source=Data\\storage.db;");
+                });
+            })
+            .Build();
     }
 
-    protected override void OnStartup(StartupEventArgs e)
+    protected override async void OnStartup(StartupEventArgs e)
     {
-        base.OnStartup(e);
+        await appHost.StartAsync();
 
-        var window = serviceProvider.GetRequiredService<MainWindow>();
+        var window = appHost.Services.GetRequiredService<MainWindow>();
         window.Show();
+
+        base.OnStartup(e);
     }
 }
