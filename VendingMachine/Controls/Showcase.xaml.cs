4573af338678c06c404aaf492fdd7ca11f970233
Nikita Khodyakov <fenyatop@gmail.com>
2022-11-27 20:59:55 +0300

    Made ItemHeight and ItemWidth bindable properties

--- VendingMachine/Controls/Showcase.xaml.cs
+++ VendingMachine/Controls/Showcase.xaml.cs
@@ -1,11 +1,47 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace VendingMachine.Controls;
 
 public partial class Showcase : UserControl
 {
+    public static readonly DependencyProperty ItemHeightProperty = DependencyProperty
+        .Register(nameof(ItemHeight), typeof(double), typeof(Showcase), new PropertyMetadata(0.0, ItemHeightPropertyChanged));
+
+    public static readonly DependencyProperty ItemWidthProperty = DependencyProperty
+        .Register(nameof(ItemWidth), typeof(double), typeof(Showcase), new PropertyMetadata(0.0, ItemWidthPropertyChanged));
+
+    public double ItemHeight
+    {
+        get => (double)GetValue(ItemHeightProperty);
+        set => SetValue(ItemHeightProperty, value);
+    }
+
+    public double ItemWidth
+    {
+        get => (double)GetValue(ItemWidthProperty);
+        set => SetValue(ItemWidthProperty, value);
+    }
+
     public Showcase()
     {
         InitializeComponent();
+    }
+
+    private static void ItemHeightPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Showcase showcase)
+            return;
+
+        showcase.ItemHeight = Convert.ToDouble(e.NewValue);
+    }
+
+    private static void ItemWidthPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is not Showcase showcase)
+            return;
+
+        showcase.ItemWidth = Convert.ToDouble(e.NewValue);
     }
 }
