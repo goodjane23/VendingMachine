a1e293e68b42fc6837f01da6eaa46296081e8bd1
GoodGateway <apochemubinet@gmail.com>
2023-01-15 18:54:00 +0300

    change pic

--- VendingMachine/Resources/Styles/MainWindow/WindowStyle.cs
+++ VendingMachine/Resources/Styles/MainWindow/WindowStyle.cs
@@ -43,31 +43,6 @@ public partial class WindowStyle
         //((Window)sender).StateChanged += WindowStateChanged;
     }
 
-    //void WindowStateChanged(object sender, EventArgs e)
-    //{
-    //    var w = ((Window)sender);
-    //    var handle = w.GetWindowHandle();
-    //    var containerBorder = (Border)w.Template.FindName("PART_Container", w);
-
-    //    if (w.WindowState == WindowState.Maximized)
-    //    {
-    //        // Make sure window doesn't overlap with the taskbar.
-    //        var screen = Screen.FromHandle(handle);
-    //        if (screen.Primary)
-    //        {
-    //            containerBorder.Padding = new Thickness(
-    //                SystemParameters.WorkArea.Left + 7,
-    //                SystemParameters.WorkArea.Top + 7,
-    //                (SystemParameters.PrimaryScreenWidth - SystemParameters.WorkArea.Right) + 7,
-    //                (SystemParameters.PrimaryScreenHeight - SystemParameters.WorkArea.Bottom) + 5);
-    //        }
-    //    }
-    //    else
-    //    {
-    //        containerBorder.Padding = new Thickness(7, 7, 7, 5);
-    //    }
-    //}
-
     private void CloseButtonClick(object sender, RoutedEventArgs e)
     {
         sender.ForWindowFromTemplate(w => SystemCommands.CloseWindow(w));
