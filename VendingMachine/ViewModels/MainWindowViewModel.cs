7ecdfbf7da10adabec1dd11fada786e34da7d204
Nikita Khodyakov <fenyatop@gmail.com>
2023-01-30 22:42:54 +0300

    Update dependencies Remove unused usings

--- VendingMachine/ViewModels/MainWindowViewModel.cs
+++ VendingMachine/ViewModels/MainWindowViewModel.cs
@@ -2,7 +2,6 @@
 using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using VendingMachine.Data.Entities;
